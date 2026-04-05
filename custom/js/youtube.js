f2e0f01bb35a3ace2602472a06ef6597807220fc
Pedro Oliveira <pncolvr@gmail.com>
2026-04-05 17:44:27 +0100

    feat: youtube enter cinema mode auto

---
+++ custom/js/youtube.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         YouTube auto cinema mode
+// @namespace    http://youtube.com/
+// @version      0.1
+// @description  YouTube auto cinema mode
+// @author       pncolvr
+// @match        https://*.youtube.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+if (document.cookie.indexOf("wide=1") === -1) {
+    if (document.cookie.indexOf("wide=1") === -1) {
+        document.cookie="wide=1;domain=.youtube.com"
+    }
+    location.reload();
+}
